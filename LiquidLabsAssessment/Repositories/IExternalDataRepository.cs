0757831c8183e0b660babba6ae77a39cf66830de
ShalaniPerera <TIQRI@DESKTOP-5L450E9>
2026-07-09 22:14:48 +0530

    create the interface repository

---
+++ LiquidLabsAssessment/Repositories/IExternalDataRepository.cs
@@ -0,0 +1,11 @@
+﻿using LiquidLabsAssessment.Models;
+
+namespace LiquidLabsAssessment.Repositories
+{
+    public interface IExternalDataRepository
+    {
+        Task<IEnumerable<ExternalDataRecord>> GetAllAsync();
+        Task<ExternalDataRecord?> GetByExternalIdAsync(int externalId);
+        Task SaveAsync(ExternalDataRecord record);
+    }
+}
