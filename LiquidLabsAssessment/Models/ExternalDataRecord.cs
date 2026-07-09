b9d9de7bd405a2a212d6484afdda0895fe4a95fb
ShalaniPerera <TIQRI@DESKTOP-5L450E9>
2026-07-09 22:00:54 +0530

    create the model

---
+++ LiquidLabsAssessment/Models/ExternalDataRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace LiquidLabsAssessment.Models
+{
+    public class ExternalDataRecord
+    {
+        public int ExternalId { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
