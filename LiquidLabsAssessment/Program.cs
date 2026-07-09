0c67c8640d29be1a3a503ee1d75c36cc8bd877cc
ShalaniPerera <TIQRI@DESKTOP-5L450E9>
2026-07-09 22:48:46 +0530

    setup the program.cs and appsetting

--- LiquidLabsAssessment/Program.cs
+++ LiquidLabsAssessment/Program.cs
@@ -1,23 +1,23 @@
+using LiquidLabsAssessment.Repositories;
+using LiquidLabsAssessment.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IExternalDataRepository, ExternalDataRepository>();
+builder.Services.AddHttpClient<IExternalDataService, ExternalDataService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
