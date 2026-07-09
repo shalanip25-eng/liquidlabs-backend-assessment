3325c1e89bcba72f5b00f226363cd40445208716
ShalaniPerera <TIQRI@DESKTOP-5L450E9>
2026-07-09 22:13:03 +0530

    create the interface service class

---
+++ LiquidLabsAssessment/Services/IExternalDataService.cs
@@ -0,0 +1,10 @@
+﻿using LiquidLabsAssessment.Models;
+
+namespace LiquidLabsAssessment.Services
+{
+    public interface IExternalDataService
+    {
+        Task<IEnumerable<ExternalDataRecord>> GetRecordsAsync();
+        Task<ExternalDataRecord?> GetRecordByIdAsync(int id);
+    }
+}
